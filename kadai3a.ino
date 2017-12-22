a02bdcad0a3a60e6293dbf54c35e725273e6cb34
taka0409 <32897506+taka0409@users.noreply.github.com>
2017-12-22 16:56:30 +0900

    Add files via upload

---
+++ kadai3a.ino
@@ -0,0 +1,12 @@
+int x;
+void setup() {
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  x=analogRead(0);
+  Serial.println(x);
+  analogWrite(3,random(20,50)+x);
+  delay(300);
+}
